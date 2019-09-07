0348406093547395a0aaf9839abe5166a0aad8e8
Mikhail Ershov <ershov@sota.ru>
2019-09-07 14:34:15 +0300

    kotlin 5, pairs and triples answer

--- src/Aquarium/Book.kt
+++ src/Aquarium/Book.kt
@@ -1,31 +1,25 @@
 package Aquarium
 
-open class Book (val title: String, val author: String, val year: Int = 1900) {
-    private var currentPage = 1
+class Book(val title: String, val author: String, val year: Int) {
 
-    open fun readPage() {
-        currentPage++
+    fun getTitleAuthor(): Pair<String, String> {
+        return (title to author)
     }
 
-    fun description(): Pair<String, String> {
-        return Pair(title, author)
-    }
-
-    fun contents(): Triple<String, String, Int> {
+    fun getTitleAuthorYear(): Triple<String, String, Int> {
         return Triple(title, author, year)
     }
 }
 
-class eBook(autor: String, titl: String, format: String = "text") : Book(author = autor, title = titl) {
-    private var wordsRead = 0
-
-    override fun readPage() {
-        wordsRead = wordsRead + 250
-    }
-}
-
 fun main(args: Array<String>) {
-   val book = Book("bout Michael Jackson", "Undefined", 2001)
-    val (title, author, year) = book.contents()
-    println("“Here is your book $title written by $author in $year")
+
+    val book = Book("Romeon and Juliet", "William Shakespeare", 1597)
+    val bookTitleAuthor = book.getTitleAuthor()
+    val bookTitleAuthorYear = book.getTitleAuthorYear()
+
+    println("Here is your book ${bookTitleAuthor.first} by ${bookTitleAuthor.second}")
+
+    println("Here is your book ${bookTitleAuthorYear.first} " +
+            "by ${bookTitleAuthorYear.second} written in ${bookTitleAuthorYear.third}")
+
 }
