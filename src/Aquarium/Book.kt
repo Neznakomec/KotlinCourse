bb7bd0b91538f642405d2ae46123cad2b277103f
Mikhail Ershov <ershov@sota.ru>
2019-09-07 14:33:20 +0300

    kotlin 5, pairs and triples

--- src/Aquarium/Book.kt
+++ src/Aquarium/Book.kt
@@ -1,10 +1,18 @@
 package Aquarium
 
-open class Book (val title: String, val author: String) {
+open class Book (val title: String, val author: String, val year: Int = 1900) {
     private var currentPage = 1
 
     open fun readPage() {
         currentPage++
+    }
+
+    fun description(): Pair<String, String> {
+        return Pair(title, author)
+    }
+
+    fun contents(): Triple<String, String, Int> {
+        return Triple(title, author, year)
     }
 }
 
@@ -14,4 +22,10 @@ class eBook(autor: String, titl: String, format: String = "text") : Book(author 
     override fun readPage() {
         wordsRead = wordsRead + 250
     }
+}
+
+fun main(args: Array<String>) {
+   val book = Book("bout Michael Jackson", "Undefined", 2001)
+    val (title, author, year) = book.contents()
+    println("“Here is your book $title written by $author in $year")
 }
