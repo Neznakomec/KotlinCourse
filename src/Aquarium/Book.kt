3a2983a85e4d0dd39050409250b1f77942f4f1ae
Mikhail Ershov <ershov@sota.ru>
2019-09-07 20:46:07 +0300

    Extension functions practice

--- src/Aquarium/Book.kt
+++ src/Aquarium/Book.kt
@@ -1,7 +1,10 @@
 package Aquarium
 
-class Book(val title: String, val author: String, val year: Int) {
+import kotlin.random.Random
 
+const val MAX_BOOKS = 5
+class Book(val title: String, val author: String, val year: Int) {
+    var pages: Int = 0
     fun getTitleAuthor(): Pair<String, String> {
         return (title to author)
     }
@@ -9,8 +12,27 @@ class Book(val title: String, val author: String, val year: Int) {
     fun getTitleAuthorYear(): Triple<String, String, Int> {
         return Triple(title, author, year)
     }
+
+    fun canBorrow(books: Int): Boolean {
+        return books < MAX_BOOKS
+    }
 }
 
+fun Book.weight(): Double = this.pages * 1.5
+
+fun Book.tornPages(count: Int) {
+    if (pages >= count) {
+        pages -= count
+    } else {
+        pages = 0
+    }
+}
+
+class Puppy {
+    fun playWithBook(book: Book) {
+        book.tornPages(Random.Default.nextInt(10))
+    }
+}
 fun main(args: Array<String>) {
 
     val book = Book("Romeon and Juliet", "William Shakespeare", 1597)
@@ -36,5 +58,10 @@ fun main(args: Array<String>) {
     moreBooks.getOrPut("Hamlet") { "Shakespeare" }
     println(moreBooks)
 
-
+    val puppy = Puppy()
+    val bookToPlay = Book("11", "22", 1990)
+    bookToPlay.pages = 100
+    println("book has ${bookToPlay.pages} pages")
+    puppy.playWithBook(bookToPlay)
+    println("book has ${bookToPlay.pages} pages")
 }
