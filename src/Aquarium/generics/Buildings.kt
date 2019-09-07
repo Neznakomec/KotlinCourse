541d8be8838c8fa1974d7b6dc2911b3bff0db89f
Mikhail Ershov <ershov@sota.ru>
2019-09-08 00:02:25 +0300

    generic function answer

--- src/Aquarium/generics/Buildings.kt
+++ src/Aquarium/generics/Buildings.kt
@@ -3,13 +3,6 @@ package Aquarium.generics
 open class BaseBuildingMaterial() {
     open val numberNeeded = 1
 
-    fun <T> BaseBuildingMaterial.isSmallBuilding(building: Building<*>) {
-        if (building.actualMaterialsNeeded < 500) {
-            println("small building")
-        } else {
-            println("large building")
-        }
-    }
 }
 
 class Wood : BaseBuildingMaterial() {
@@ -31,10 +24,12 @@ class Building<out T: BaseBuildingMaterial>(val buildingMaterial: T) {
 }
 
 
-/*
-Create a generic function for type BaseBuildingMaterial and call it isSmallBuilding, which takes a Building with a building material T as an argument. If the materials needed are less than 500, print "small building", otherwise, print "large building".
- */
+fun <T : BaseBuildingMaterial> isSmallBuilding(building: Building<T>) {
+    if (building.actualMaterialsNeeded < 500) println("Small building")
+    else println("large building")
+}
 
 fun main(args: Array<String>) {
     Building(Wood()).build()
+    isSmallBuilding(Building(Brick()))
 }
