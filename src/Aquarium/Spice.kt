7c8fb0d23b186f0140603a318647dd6d9417f7da
Mikhail Ershov <ershov@sota.ru>
2019-09-07 09:47:13 +0300

    Spices answer

--- src/Aquarium/Spice.kt
+++ src/Aquarium/Spice.kt
@@ -1,22 +1,28 @@
 package Aquarium
 
-class Spice(val name: String, val level: String = "mild") {
+class Spice(val name: String, val spiciness: String = "mild") {
+
     val heat: Int
         get() {
-            when (level) {
-                "mild" -> return 5
-                else -> return 0
+            return when (spiciness) {
+                "mild" -> 1
+                "medium" -> 3
+                "spicy" -> 5
+                "very spicy" -> 7
+                "extremely spicy" -> 10
+                else -> 0
             }
         }
-
-    init {
-        println("it's $name spice with $heat level of heat")
-    }
 }
-
-val lst = listOf<Spice>(Spice("hh"),
-    Spice("gg, 0"))
-
-val lstFileterd = lst.filter { spice -> spice.heat > 0 }
-
-fun makeSalt() = Spice("salt", "curry")
+val spices1 = listOf(
+    Spice("curry", "mild"),
+    Spice("pepper", "medium"),
+    Spice("cayenne", "spicy"),
+    Spice("ginger", "mild"),
+    Spice("red curry", "medium"),
+    Spice("green curry", "mild"),
+    Spice("hot pepper", "extremely spicy")
+)
+val spice = Spice("cayenne", spiciness = "spicy")
+val spicelist = spices1.filter {it.heat < 5}
+fun makeSalt() = Spice("Salt")
